2b2930a939286bbbb03272c5c15964d10f49ba9e
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-03 16:28:28 +0200

    Get ViewFlipper reference and set OnTouchListener

--- app/src/main/java/pl/kata/bielanski/viewflipperkata/MainActivity.java
+++ app/src/main/java/pl/kata/bielanski/viewflipperkata/MainActivity.java
@@ -2,15 +2,28 @@ package pl.kata.bielanski.viewflipperkata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ViewFlipper;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+	@Bind(R.id.viewFlipper)
+	ViewFlipper viewFlipper;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
+
+		viewFlipper.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return false;
+			}
+		});
 	}
 }
